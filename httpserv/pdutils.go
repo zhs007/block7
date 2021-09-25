698e231e6ecec273ce7a5e7494819478f02c501c
zhs007 <sssxueren@gmail.com>
2021-09-25 09:59:13 +0000

    Added UpdUserDataParams2PB

--- httpserv/pdutils.go
+++ httpserv/pdutils.go
@@ -19,3 +19,51 @@ func LoginParams2PB(params *LoginParams) *block7pb.UserDeviceInfo {
 
 	return udi
 }
+
+func UpdUserDataParams2PB(params *UpdUserDataParams, uds *UpdUserDataStatus) *block7pb.UserData {
+	ud := &block7pb.UserData{
+		Name:     params.Name,
+		Platform: params.Platform,
+		Version:  params.Version,
+	}
+
+	if uds.HasCoin {
+		ud.Coin = params.Coin
+	}
+
+	if uds.HasCooking {
+		for _, v := range params.Cooking {
+			c := &block7pb.Cooking{
+				Level:    int32(v.Level),
+				Unlock:   v.Unlock,
+				StarNums: int32(v.StarNums),
+			}
+
+			ud.Cooking = append(ud.Cooking, c)
+		}
+	}
+
+	if uds.HasLevel {
+		ud.Level = int32(params.Level)
+	}
+
+	if uds.HasHomeScene {
+		for _, v := range params.HomeScene {
+			ud.HomeScene = append(ud.HomeScene, int32(v))
+		}
+	}
+
+	if uds.HasLevelArr {
+		for k, v := range params.LevelArr {
+			ud.LevelArr[int32(k)] = int32(v)
+		}
+	}
+
+	if uds.HasToolsArr {
+		for k, v := range params.ToolsArr {
+			ud.ToolsArr[int32(k)] = int32(v)
+		}
+	}
+
+	return ud
+}
